819120f210f83f476a3b6d6bb4fbe7bc138c4feb
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-12 13:50:10 -0600

    extra

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
@@ -11,6 +12,20 @@ public class Input {
         System.out.println("Contains numbers? " + StringUtils.isNumeric(input));
         System.out.println("Flipped Case: "+ StringUtils.swapCase(input));
         System.out.println("Reversed: "+StringUtils.reverse(input));
+        System.out.println();
+//        System.out.println("enter name");
+//        Scanner pc =new Scanner(System.in);
+//        String names= pc.nextLine();
+        Faker faker = new Faker();
 
+        String name = faker.name().fullName(); // Miss Samanta Schmidt
+        String firstName = faker.name().firstName(); // Emory
+        String lastName = faker.name().lastName(); // Barton
+
+        String streetAddress = faker.address().streetAddress();
+        System.out.println(name);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
     }
 }
